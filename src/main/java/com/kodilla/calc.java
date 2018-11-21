037a042d2c6958364374b8b08da7cec841f8fd61
AdasioH <adam.halas.notes@gmail.com>
2018-11-21 15:57:17 +0100

    Kalkulator

--- src/main/java/com/kodilla/calc.java
+++ src/main/java/com/kodilla/calc.java
@@ -1,4 +1,23 @@
 package com.kodilla;
 
-public class calc {
+
+import java.lang.*;
+
+
+class Calc {
+    int a;
+    int b;
+    public Calc(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void add(){
+        System.out.println(a+b);
+    }
+    public static void main(String args[]){
+
+        Calc calc = new Calc(3, 1);
+        calc.add();
+    }
 }
